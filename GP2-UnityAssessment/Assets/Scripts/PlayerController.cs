f3f1bb091079b88ac726d64852ef55a70f5b652a
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2019-05-06 12:46:53 +0100

    Added Player + UI Rough

---
+++ GP2-UnityAssessment/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float bSpeed = 10f;
+    float Speed;
+    public float Sprint = 5f;
+    private Vector3 _movement;
+    private Rigidbody _playerRigidBody;
+
+ 
+
+    private void Awake()
+    {
+        _playerRigidBody = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Speed = Sprint;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            Speed = bSpeed;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Move(horizontal, vertical);
+    }
+    
+    private void Move(float horizontal, float vertical)
+    {
+        _movement = (vertical* transform.forward) + (horizontal* transform.right);
+        _movement = _movement.normalized* Speed * Time.deltaTime;
+        _playerRigidBody.MovePosition(transform.position + _movement);
+    }
+}
